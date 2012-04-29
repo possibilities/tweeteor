6a98ba831d21301185788f8cd862126ba197c321
Mike Bannister <notimpossiblemike@gmail.com>
2012-04-28 23:36:52 -0400

    added comment to run_method_every

--- server/lib/run_method_every.js
+++ server/lib/run_method_every.js
@@ -1,3 +1,4 @@
+// just a setInterval that runs the method once before kicking things off
 var runMethodEvery = function(func, timeout) {
   func();
   func.runId = func.runId || Meteor.setInterval(func, timeout);
