0e265088b69f4cd903554a78d85f09a3d5c219b8
Mike Bannister <notimpossiblemike@gmail.com>
2012-04-28 23:58:53 -0400

    move timefix

--- client/tweeteor.js
+++ client/tweeteor.js
@@ -4,24 +4,24 @@ Template.public_timeline.tweets = function () {
 };
 
 Template.public_timeline.updatedAt = function() {
-  return appInfo.get('updatedAt');
+  return timeFix(appInfo.get('updatedAt'));
 };
 
 var timeFix = function(time) {
   // meteor.com server time drifts into the future
   // for now don't mess with times from the future
-  if (new Date(time) > new Date) time = new Date;
+  if (new Date(time) >= new Date) time = new Date;
   return time;
 };
 
 Handlebars.registerHelper('timeAgoFormat', function(dateString) {
-  var time = timeFix(new Date(dateString));
+  var time = new Date(dateString);
   return humanized_time_span(dateString);
 });
 
 var showFriendlyTime = function() {
   var updatedAtDate = $('#updatedAt').attr('data-updated-at');
-  $('#updatedAt').text(humanized_time_span(timeFix(updatedAtDate)));
+  $('#updatedAt').text(humanized_time_span(updatedAtDate));
 };
 
 Meteor.setInterval(showFriendlyTime, 5000);
